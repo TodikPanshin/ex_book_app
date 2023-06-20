61e14d620ed0dc9c2ff7883cd46170b4a644176c
ohadMazza <ohadmazza@gmail.com>
2023-06-20 17:28:36 +0300

    fix layout

--- src/cmps/books.jsx
+++ src/cmps/books.jsx
@@ -34,29 +34,34 @@ export function Books() {
     console.log('test book', book)
     return (
         <>
+            <div className='book-container'>
+                <button onClick={() => handaleBookIdx(-1)}>back </button>
+                <article className="book-card">
+                    <div className="card-header">
+                        <h3 className='book-title'>{book.title}</h3>
+                        <input
+                            type="checkbox"
+                            id=""
+                            name=""
+                            checked={book.isFavorite}
+                            onChange={handleCheckboxChange}
+                        />
+                    </div>
+                    <hr></hr>
+                    <h3 className='book-author'>{book.author}</h3>
+                    <p>{book.description}</p>
+                    <div className='rating'>
+                        <span>Rating:</span>
+                        <div>stars cmp</div>
+                    </div>
+                    <h3 className='price'>Price: $<span>{book.price}</span> </h3>
+                </article >
+                <button onClick={() => handaleBookIdx(+1)}>next</button>
 
-            <article className="book-card">
-                <div className="card-header">
-                    <h3>{book.title}</h3>
-                    <input
-                        type="checkbox"
-                        id=""
-                        name=""
-                        checked={book.isFavorite}
-                        onChange={handleCheckboxChange}
-                    />
-                </div>
-                <hr></hr>
-                <h3>{book.author}</h3>
-                <p>{book.description}</p>
-                <div>
-                    <span>Rating:</span>
-                    <div>stars cmp</div>
-                </div>
-                <h3>Price: $<span>{book.price}</span> </h3>
-            </article >
-            <button onClick={() => handaleBookIdx(-1)}>back </button>
-            <button onClick={() => handaleBookIdx(+1)}>next</button>
+
+
+            </div>
+
         </>
     )
 }
