b9feb3fa6c3b0d189b6fc2ac6284c2fd5d0392a8
ohadMazza <ohadmazza@gmail.com>
2023-06-20 14:44:13 +0300

    add store

---
+++ src/store/books.actions.js
@@ -0,0 +1,23 @@
+import { booksService } from '../services/books.service.js'
+import { store } from './store.js'
+
+import {
+    SET_BOOKS,
+} from './books.reducer.js'
+
+// Action Creators:
+
+export async function loadBooks() {
+    try {
+        const books = await booksService.query()
+        store.dispatch({
+            type: SET_BOOKS,
+            boards,
+        })
+    } catch (err) {
+        console.log('Cannot load books', err)
+        throw err
+    }
+}
+
+
